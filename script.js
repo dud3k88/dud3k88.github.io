1e43c44b97da82b5115e0c368dc6816d70fe98d6
dud3k88 <mateusz.dudek88@gmail.com>
2020-11-28 21:56:12 +0100

    next

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://secret-lowlands-18866.herokuapp.com/v1/task';
+  const apiRoot = 'https://secret-lowlands-18866.herokuapp.com/v1/task/';
   const trelloApiRoot = 'http://localhost:8080/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
