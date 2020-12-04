32fccd0fb34e09a839754878beace4a6b13bc052
dud3k88 <mateusz.dudek88@gmail.com>
2020-12-04 13:50:29 +0100

    comit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://calm-chamber-68967.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const trelloApiRoot = 'https://calm-chamber-68967.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
